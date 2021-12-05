9fd71a9e190a62943604c7d44ebc3764278a947e
John <john@jbrereton.com>
2021-12-05 17:24:25 -0500

    Create RRBotAutonomous.java

---
+++ RRBotAutonomous.java
@@ -0,0 +1,198 @@
+/* Copyright (c) 2017 FIRST. All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted (subject to the limitations in the disclaimer below) provided that
+ * the following conditions are met:
+ *
+ * Redistributions of source code must retain the above copyright notice, this list
+ * of conditions and the following disclaimer.
+ *
+ * Redistributions in binary form must reproduce the above copyright notice, this
+ * list of conditions and the following disclaimer in the documentation and/or
+ * other materials provided with the distribution.
+ *
+ * Neither the name of FIRST nor the names of its contributors may be used to endorse or
+ * promote products derived from this software without specific prior written permission.
+ *
+ * NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+ * LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+ * THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.util.ElapsedTime;
+import com.qualcomm.robotcore.util.Range;
+
+
+/**
+ * This file contains an minimal example of a Linear "OpMode". An OpMode is a 'program' that runs in either
+ * the autonomous or the teleop period of an FTC match. The names of OpModes appear on the menu
+ * of the FTC Driver Station. When an selection is made from the menu, the corresponding OpMode
+ * class is instantiated on the Robot Controller and executed.
+ *
+ * This particular OpMode just executes a basic Tank Drive Teleop for a two wheeled robot
+ * It includes all the skeletal structure that all linear OpModes contain.
+ *
+ * Use Android Studios to Copy this Class, and Paste it into your team's code folder with a new name.
+ * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list
+ */
+
+@Autonomous(name="Basic: Linear OpMode", group="Linear Opmode")
+public class RRBotAutonomous extends LinearOpMode {
+
+    // Declare OpMode members.
+    RRBotHardware robot = new RRBotHardware();
+    RRBotMecanumDrive drive = new RRBotMecanumDrive(robot);
+    private ElapsedTime runtime = new ElapsedTime();
+
+    private final double COUNTS_PER_MOTOR_REV = 560 ;    // Andymark 20:1 gearmotor
+    private final double DRIVE_GEAR_REDUCTION = 2.0 ;     // This is < 1.0 if geared UP
+    private final double WHEEL_DIAMETER_INCHES = 4.0 ;     // For figuring circumference
+    private final double COUNTS_PER_INCH = (COUNTS_PER_MOTOR_REV * DRIVE_GEAR_REDUCTION) / (WHEEL_DIAMETER_INCHES * 3.1415);
+
+    //gyro variables
+    private BNO055IMU imu;
+    private Orientation angles;
+
+    @Override
+    public void runOpMode() {
+        telemetry.addData("Status", "Initialized");
+        telemetry.update();
+
+        // Wait for the game to start (driver presses PLAY)
+        waitForStart();
+        runtime.reset();
+
+        // run until the end of the match (driver presses STOP)
+        while (opModeIsActive()) {
+
+
+
+            // Show the elapsed game time and wheel power.
+            telemetry.addData("Status", "Run Time: " + runtime.toString());
+            telemetry.update();
+        }
+    }
+
+    public void EncoderDriveTank(double speed, double leftInches, double rightInches, double timeoutS)
+    {
+        robot.rearLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.rearRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.frontLeftDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        robot.frontRightDrive.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        idle();
+
+        robot.rearLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.rearRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.frontLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        robot.frontRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+
+        int newRearLeftTarget;
+        int newRearRightTarget;
+        int newFrontLeftTarget;
+        int newFrontRightTarget;
+
+        // Ensure that the opmode is still active
+        if (opModeIsActive())
+        {
+            // Determine new target position, and pass to motor controller
+            newRearLeftTarget = robot.rearLeftDrive.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH);
+            newRearRightTarget = robot.rearRightDrive.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
+            newFrontLeftTarget = robot.frontLeftDrive.getCurrentPosition() + (int) (leftInches * COUNTS_PER_INCH);
+            newFrontRightTarget = robot.frontRightDrive.getCurrentPosition() + (int) (rightInches * COUNTS_PER_INCH);
+            robot.rearLeftDrive.setTargetPosition(newRearLeftTarget);
+            robot.rearRightDrive.setTargetPosition(newRearRightTarget);
+            robot.frontLeftDrive.setTargetPosition(newFrontLeftTarget);
+            robot.frontRightDrive.setTargetPosition(newFrontRightTarget);
+
+            // Turn On RUN_TO_POSITION
+            robot.rearLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.rearRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.frontLeftDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            robot.frontRightDrive.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+
+            // reset the timeout time and start motion.
+            runtime.reset();
+            robot.rearLeftDrive.setPower(Math.abs(speed));
+            robot.rearRightDrive.setPower(Math.abs(speed));
+            robot.frontLeftDrive.setPower(Math.abs(speed));
+            robot.frontRightDrive.setPower(Math.abs(speed));
+
+            // keep looping while we are still active, and there is time left, and both motors are running.
+            while(opModeIsActive() &&
+                    (runtime.seconds() < timeoutS) &&
+                    (robot.rearLeftDrive.isBusy() && robot.rearRightDrive.isBusy() && robot.frontLeftDrive.isBusy() && robot.frontRightDrive.isBusy()))
+            {
+
+                // Display it for the driver.
+                telemetry.addData("Path1", "Running to %7d :%7d", newRearLeftTarget, newRearRightTarget, newFrontLeftTarget, newFrontRightTarget);
+                telemetry.addData("Path2", "Running at %7d :%7d",
+                        robot.rearLeftDrive.getCurrentPosition(),
+                        robot.rearRightDrive.getCurrentPosition(),
+                        robot.frontLeftDrive.getCurrentPosition(),
+                        robot.frontRightDrive.getCurrentPosition());
+                telemetry.update();
+            }
+
+            TurnOffMotors();
+
+            // Turn off RUN_TO_POSITION
+            robot.rearLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.rearRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.frontLeftDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+            robot.frontRightDrive.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        }
+    }
+
+    public void TurnByGyro(String direction, int angle, double speed)
+    {
+        angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+        float startHeading = angles.firstAngle;
+
+        if(direction.equals("left"))
+        {
+            robot.rearRightDrive.setPower(speed);
+            robot.rearLeftDrive.setPower(-speed);
+            robot.frontRightDrive.setPower(speed);
+            robot.frontLeftDrive.setPower(-speed);
+        }
+        else if(direction.equals("right"))
+        {
+            robot.rearRightDrive.setPower(-speed);
+            robot.rearLeftDrive.setPower(speed);
+            robot.frontRightDrive.setPower(-speed);
+            robot.frontLeftDrive.setPower(speed);
+        }
+
+        while(opModeIsActive() && Math.abs(angles.firstAngle - startHeading) < angle)
+        {
+            angles = imu.getAngularOrientation(AxesReference.INTRINSIC, AxesOrder.ZYX, AngleUnit.DEGREES);
+            telemetry.addData("heading", formatAngle(AngleUnit.DEGREES, angles.firstAngle));
+            telemetry.update();
+        }
+
+        TurnOffMotors();
+    }
+
+    public void TurnOffMotors()
+    {
+        robot.rearRightDrive.setPower(0);
+        robot.rearLeftDrive.setPower(0);
+        robot.frontRightDrive.setPower(0);
+        robot.frontLeftDrive.setPower(0);
+    }
+}

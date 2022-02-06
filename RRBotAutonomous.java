5dacb5bc67c7e65bcbdffa512aed1673b6ac11ce
John <john@jbrereton.com>
2022-02-06 14:35:38 -0500

    Update RRBotAutonomous.java

--- RRBotAutonomous.java
+++ RRBotAutonomous.java
@@ -2,6 +2,7 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.util.ElapsedTime;
 import java.util.List;
 import org.firstinspires.ftc.robotcore.external.ClassFactory;
 import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer;
@@ -9,29 +10,8 @@ import org.firstinspires.ftc.robotcore.external.navigation.VuforiaLocalizer.Came
 import org.firstinspires.ftc.robotcore.external.tfod.TFObjectDetector;
 import org.firstinspires.ftc.robotcore.external.tfod.Recognition;
 
-/**
- * This 2020-2021 OpMode illustrates the basics of using the TensorFlow Object Detection API to
- * determine the position of the Freight Frenzy game elements.
- *
- * Use Android Studio to Copy this Class, and Paste it into your team's code folder with a new name.
- * Remove or comment out the @Disabled line to add this opmode to the Driver Station OpMode list.
- *
- * IMPORTANT: In order to use this OpMode, you need to obtain your own Vuforia license key as
- * is explained below.
- */
 @Autonomous(name = "RRBotAutonomous")
 public class RRBotAutonomous extends LinearOpMode {
-    /* Note: This sample uses the all-objects Tensor Flow model (FreightFrenzy_BCDM.tflite), which contains
-     * the following 4 detectable objects
-     *  0: Ball,
-     *  1: Cube,
-     *  2: Duck,
-     *  3: Marker (duck location tape marker)
-     *
-     *  Two additional model assets are available which only contain a subset of the objects:
-     *  FreightFrenzy_BC.tflite  0: Ball,  1: Cube
-     *  FreightFrenzy_DM.tflite  0: Duck,  1: Marker
-     */
     private static final String TFOD_MODEL_ASSET = "FreightFrenzy_BCDM.tflite";
     private static final String[] LABELS = {
             "Ball",
@@ -40,102 +20,106 @@ public class RRBotAutonomous extends LinearOpMode {
             "Marker"
     };
 
-    /*
-     * IMPORTANT: You need to obtain your own license key to use Vuforia. The string below with which
-     * 'parameters.vuforiaLicenseKey' is initialized is for illustration only, and will not function.
-     * A Vuforia 'Development' license key, can be obtained free of charge from the Vuforia developer
-     * web site at https://developer.vuforia.com/license-manager.
-     *
-     * Vuforia license keys are always 380 characters long, and look as if they contain mostly
-     * random data. As an example, here is a example of a fragment of a valid key:
-     *      ... yIgIzTqZ4mWjk9wd3cZO9T1axEqzuhxoGlfOOI2dRzKS4T0hQ8kT ...
-     * Once you've obtained a license key, copy the string from the Vuforia web site
-     * and paste it in to your code on the next line, between the double quotes.
-     */
     private static final String VUFORIA_KEY = "AY+9b5H/////AAABmdP4VHcJckRZm8RJLoHJr6ZCdCwkvYa4e33vyEGuyyl/foBfTYRNT52OO+pJ+FP60SP1HncEL5fgHmD3lbe5XWqlqUt3a6y5hAXpuEDutdVo/n77+mI58Af9ZaBvv9cD2+eXKvwZrFDAEmgZ/+I4OWglTyO2u+zJSNWzA2dLEzM0sPCECY6wR3ytsbff21SAY1MBmVGVjFiAumcc4bdOapJRqXoKHywtduws9uCC3piJGMCqPZmqBTUOnR7myumXyZZWL4TQKfYkcsEKrrlReY0iOgdbTxvDrriljP/FqcoY9UFGenaT6oZ3+/DdfWE+fiarCSzoUdJy+h2BkamY8K4ehsk/bSKG0+qbC8IrQFUl";
 
-    /**
-     * {@link #vuforia} is the variable we will use to store our instance of the Vuforia
-     * localization engine.
-     */
-    private VuforiaLocalizer vuforia;
+    private VuforiaLocalizer vuforia; // Local instance of Vuforia
 
-    /**
-     * {@link #tfod} is the variable we will use to store our instance of the TensorFlow Object
-     * Detection engine.
-     */
-    private TFObjectDetector tfod;
+    private TFObjectDetector tfod; // Local instance of TensorFlow Object
+
+    private ElapsedTime runtime = new ElapsedTime();
 
     @Override
     public void runOpMode() {
-        // The TFObjectDetector uses the camera frames from the VuforiaLocalizer, so we create that
-        // first.
         initVuforia();
         initTfod();
 
-        /**
-         * Activate TensorFlow Object Detection before we wait for the start command.
-         * Do it here so that the Camera Stream window will have the TensorFlow annotations visible.
-         **/
+        // Activate TensorFlow Object Detection
         if (tfod != null) {
             tfod.activate();
 
-            // The TensorFlow software will scale the input images from the camera to a lower resolution.
-            // This can result in lower detection accuracy at longer distances (> 55cm or 22").
-            // If your target is at distance greater than 50 cm (20") you can adjust the magnification value
-            // to artificially zoom in to the center of image.  For best results, the "aspectRatio" argument
-            // should be set to the value of the images used to create the TensorFlow Object Detection model
-            // (typically 16/9).
-            tfod.setZoom(1, 16.0/9.0);
+            tfod.setZoom(2, 16.0/9.0);
         }
 
         /** Wait for the game to begin */
-        telemetry.addData(">", "Press Play to start op mode");
+        telemetry.addData("Status", "Robot initialized, locating object");
         telemetry.update();
+
+        int objectPos = getObjectPos();
+
+        telemetry.addData("Status", "Robot initialized, object located");
+
         waitForStart();
 
+
+
         if (opModeIsActive()) {
-            while (opModeIsActive()) {
-                if (tfod != null) {
-                    // getUpdatedRecognitions() will return null if no new information is available since
-                    // the last time that call was made.
-                    List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
-                    if (updatedRecognitions != null) {
-                        telemetry.addData("# Object Detected", updatedRecognitions.size());
-                        
-                        int objectPos;
-                        if (updatedRecognitions.size() == 1) {
-                            objectPos = (int) updatedRecognitions.get(0).getLeft();
 
-                            telemetry.addData("Object Detection", objectPos);
-                        }
-                        else if (updatedRecognitions.size() == 0){
-                            telemetry.addData("Object Detection", "No object detected");
-                        }
-                        else {
-                            telemetry.addData("Object Detection", "More than one object detected");
-                            
-                            int object = 0;
-                            float highestConfidence = 0;
-                            int i = 0;
-                            for (Recognition recognition : updatedRecognitions) {
-                                if (recognition.getConfidence() > highestConfidence) {
-                                    object = i;
-                                    highestConfidence = recognition.getConfidence();
-                                }
-                                i++;
+        }
+    }
+
+    public int getObjectPos() {
+        runtime.reset();
+
+        int objectY = 0;
+        int objectPos = 0;
+
+        while (runtime.seconds() > 5) {
+            if (tfod != null) {
+                // getUpdatedRecognitions() will return null if no new information is available since
+                // the last time that call was made.
+                List<Recognition> updatedRecognitions = tfod.getUpdatedRecognitions();
+                if (updatedRecognitions != null) {
+                    telemetry.addData("# Object Detected", updatedRecognitions.size());
+
+                    // Get y position of recognized object with highest confidence
+                    boolean objectDetected = false;
+                    if (updatedRecognitions.size() == 1) {
+                        objectY = (int) updatedRecognitions.get(0).getLeft();
+
+                        objectDetected = true;
+
+                        telemetry.addData("Object Detection", objectY);
+                    } else if (updatedRecognitions.size() == 0) {
+                        telemetry.addData("Object Detection", "No object detected");
+                    } else {
+                        telemetry.addData("Object Detection", "More than one object detected");
+
+                        objectDetected = true;
+
+                        int object = 0;
+                        float highestConfidence = 0;
+                        int i = 0;
+                        for (Recognition recognition : updatedRecognitions) {
+                            if (recognition.getConfidence() > highestConfidence) {
+                                object = i;
+                                highestConfidence = recognition.getConfidence();
                             }
-                            
-                            objectPos = (int) updatedRecognitions.get(object).getLeft();
-                            telemetry.addData("Object Detection", objectPos);
+                            i++;
                         }
 
-                        telemetry.update();
-
+                        objectY = (int) updatedRecognitions.get(object).getLeft();
+                        telemetry.addData("Object Detection", objectY);
                     }
+                    
+                    if (!objectDetected) {
+                        objectPos = 0;
+                        telemetry.addData("Target Location", "[\uD83D\uDC24][   ][   ]");
+                    } else if (objectDetected && objectY > 600) {
+                        objectPos = 1;
+                        telemetry.addData("Target Location", "[   ][\uD83D\uDC24][   ]");
+                    } else if (objectDetected) {
+                        objectPos = 2;
+                        telemetry.addData("Target Location", "[   ][   ][\uD83D\uDC24]");
+                    } else {
+                        telemetry.addData("Target Location", "[   ][   ][   ]");
+                    }
+
+                    telemetry.update();
+
                 }
             }
         }
+        return objectPos;
     }
 
     /**
@@ -152,8 +136,6 @@ public class RRBotAutonomous extends LinearOpMode {
 
         //  Instantiate the Vuforia engine
         vuforia = ClassFactory.getInstance().createVuforia(parameters);
-
-        // Loading trackables is not necessary for the TensorFlow Object Detection engine.
     }
 
     /**

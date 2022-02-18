233212530d3e85b8893f26ed8972e9bf26b48614
Barrel <visvam64@gmail.com>
2022-02-17 19:47:51 -0500

    Update RRBotAutoTestPath.java

--- RRBotAutoTestPath.java
+++ RRBotAutoTestPath.java
@@ -115,9 +115,9 @@ public class RRBotAutoTestPath extends LinearOpMode {
 
         // Step through each leg of the path,
         // Note: Reverse movement is obtained by setting a negative distance (not speed)
-        encoderDrive(DRIVE_SPEED,  24,  24, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
-        encoderDrive(TURN_SPEED,   12, -12, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
-        encoderDrive(DRIVE_SPEED, -24, -24, 4.0);  // S3: Reverse 24 Inches with 4 Sec timeout
+        encoderDrive(DRIVE_SPEED,  48,  48, 5.0);  // S1: Forward 47 Inches with 5 Sec timeout
+        //encoderDrive(TURN_SPEED,   12, -12, 4.0);  // S2: Turn Right 12 Inches with 4 Sec timeout
+        //encoderDrive(DRIVE_SPEED, -24, -24, 4.0);  // S3: Reverse 24 Inches with 4 Sec timeout
 
         robot.armMotor.setTargetPosition(3800);            // S4: Stop and close the claw.
         robot.intakePusher.setPosition(0);
